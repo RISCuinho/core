7f516b7e3b300c9048a37da028ff1a4dac334710
Carlos Delfino (WSL - Estudio - 4) <consultoria@carlosdelfino.eti.br>
2021-03-20 01:02:47 -0300

    Adotado arquivo de configuração

--- RISCuin.v
+++ RISCuin.v
@@ -1,22 +1,4 @@
-`ifdef __ICARUS__
-`define SIMULATOR 1
-`elsif __YOSYS__
-`define SIMULATOR 1
-`else 
-`define DATA_MEMORY_WIDTH 10
-`endif 
-
-`ifdef SIMULATOR
-`define MEM_DATA_ADDR_WIDTH 10
-`define MEM_DATA_WIDTH 32
-`define INTERNAL_DATA_WIDTH 32 
-`define INSTR_ADDR_WIDTH 5
-`else
-`define MEM_DATA_ADDR_WIDTH 10
-`define MEM_DATA_WIDTH 32
-`define INTERNAL_DATA_WIDTH 32 
-`define INSTR_ADDR_WIDTH 5
-`endif
+`include "./config.vh"
 
 module RISCuin(
    input clk, rst, 
---
+++ config.vh
@@ -0,0 +1,20 @@
+// Configuration File
+`ifdef __ICARUS__
+`define SIMULATOR 1
+`elsif __YOSYS__
+`define SIMULATOR 1
+`else 
+`define DATA_MEMORY_WIDTH 10
+`endif 
+
+`ifdef SIMULATOR
+`define MEM_DATA_ADDR_WIDTH 10
+`define MEM_DATA_WIDTH 32
+`define INTERNAL_DATA_WIDTH 32 
+`define INSTR_ADDR_WIDTH 5
+`else
+`define MEM_DATA_ADDR_WIDTH 10
+`define MEM_DATA_WIDTH 32
+`define INTERNAL_DATA_WIDTH 32 
+`define INSTR_ADDR_WIDTH 5
+`endif
