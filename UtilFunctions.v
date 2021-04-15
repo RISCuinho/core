ecda9784524c2484cc788f179a0e71edec9ae890
Carlos Delfino (WSL - Estudio - 4) <consultoria@carlosdelfino.eti.br>
2021-04-15 00:17:50 -0300

    Inciando a colecionar funções uteis e de exemplos

---
+++ UtilFunctions.v
@@ -0,0 +1,11 @@
+//from IEEE 1364-2001 Std
+function integer clogb2;
+    input [31:0] value;
+    begin
+        value = value - 1;
+        for (clogb2 = 0; value > 0; clogb2 = clogb2 + 1) begin
+            value = value >> 1;
+        end
+    end
+endfunction
+
