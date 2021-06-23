716c6a605e70deca22625c188428c1000dc95131
Carlos Delfino - Acer - Estudio 3 <consultoria@carlosdelfino.eti.br>
2021-06-22 21:32:56 -0300

    parametrização e correção de tabulação

--- src/RISCUIN_Quartus.v
+++ src/RISCUIN_Quartus.v
@@ -1,11 +1,11 @@
 module RISCUIN_Quartus(
-	input 		          		CLOCK_50,
-	output		     [7:0]		LED
+   input             CLOCK_50,
+   output           [7:0]     LED
 );
 
-	wire rst;
+   wire rst;
 
-	AutoReset aRst(.clk(CLOCK_50), .count(35), .rst(rst)); 
+   AutoReset aRst(.clk(CLOCK_50), .count(35), .rst(rst)); 
    RISCuin cpu(.clk(CLOCK_50), .rst(rst), .pc_end(LED[7]));
 
 
--- src/RISCuin_tb.v
+++ src/RISCuin_tb.v
@@ -4,38 +4,38 @@
 
 module RISCuin_tb;
 
-	reg clk = 1'b0;
+   reg clk = 1'b0;
 
 `ifndef __YOSYS__
-	initial 
-	begin
-		forever 
-			#2 clk = !clk;
-	end
+   initial 
+   begin
+      forever 
+         #2 clk = !clk;
+   end
 `endif 
 
 
-	initial 
-	begin
-		#00000 $display("Iniciado!");
-		
-		$dumpfile("RISCuin.vcd");
-		$dumpvars;
+   initial 
+   begin
+      #00000 $display("Iniciado!");
+      
+      $dumpfile("RISCuin.vcd");
+      $dumpvars;
 
-		#01500 $display("1500 Ticks será finalziado!");
-		$finish;
-	 end
+      #01500 $display("1500 Ticks será finalziado!");
+      $finish;
+    end
 
-	wire  pc_end;
+   wire  pc_end;
 
 `ifndef __YOSYS__
-	always @(posedge pc_end)
-		if(pc_end) $finish;
+   always @(posedge pc_end)
+      if(pc_end) $finish;
 `endif 
 
-	wire rst;
-	
-	AutoReset aRst(.clk(clk), .count(30), .rst(rst)); 
+   wire rst;
+   
+   AutoReset aRst(.clk(clk), .count(30), .rst(rst)); 
    RISCuin cpu(.clk(clk), .rst(rst), .pc_end(pc_end));
 
 endmodule
