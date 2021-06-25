5c07672348052d0d8ffb095d57983af6c10681d8
Carlos Delfino - Acer - Estudio 3 <consultoria@carlosdelfino.eti.br>
2021-06-24 22:03:27 -0300

    Finalizada a nova abordagem da ULA com carrige
    Ainda em testes

--- src/IntegerBasicALU.v
+++ src/IntegerBasicALU.v
@@ -7,36 +7,36 @@ module IntegerBasicALU #(
    input         [ 5:0]           alu_op,
    input  signed [DATA_WIDTH-1:0] A, B,
    output signed [DATA_WIDTH-1:0] out,
-   output                         carriage 
+   output                         carry
 );
 
-wire [DATA_WIDTH-1:0] left,right,arith; 
-
-assign {carrige,left}  = alu_op == `ALU_OP_SHIFT_LEFT    ? $signed(A)         << $signed(B) : {DATA_WIDTH + 1{1'b0}};        
-assign {right,carrige} = alu_op == `ALU_OP_SHIFT_RIGHT   ? $signed(A)         >> $signed(B) : 
-                         alu_op == `ALU_OP_SHIFT_RIGHT_A ? $signed($signed(A) >>> B) :        {DATA_WIDTH + 1{1'b0}};        
-
-assign {carrige,arith} = alu_op == `ALU_OP_PLUS          ? $signed(A) + $signed(B)   :
-                         alu_op == `ALU_OP_SUB           ? $signed(A) - $signed(B)   :        {DATA_WIDTH + 1{1'b0}};        
-
-assign out = !E                                 ? {DATA_WIDTH{1'b0}}        :
+assign {left,out,right} = !E                   ? {DATA_WIDTH+2{1'b0}}             :
                
-             alu_op == `ALU_OP_PLUS || `ALU_OP_SUB ? arith :  
+             alu_op == `ALU_OP_PLUS            ? {{1'b0,$signed(A)} + $signed(B),1'b0}   :
+             alu_op == `ALU_OP_SUB             ? {{1'b0,$signed(A)} - $signed(B),1'b0}   :
 
-             alu_op == `ALU_OP_SHIFT_LEFT       ? left   :  
-             alu_op == `ALU_OP_SHIFT_RIGHT || `ALU_OP_SHIFT_RIGHT_A    ? right  :
+             alu_op == `ALU_OP_SHIFT_LEFT      ? {{1'b0,$signed(A)} << $signed(B),1'b0}  :         
 
-             alu_op == `ALU_OP_SET_LESS_THAN_U ?  A < B                    :
+             alu_op == `ALU_OP_SHIFT_RIGHT     ? {1'b0,{        $signed(A),1'b0} >>  $signed(B)}  : 
+             alu_op == `ALU_OP_SHIFT_RIGHT_A   ? {1'b0,$signed({$signed(A),1'b0} >>> B)}          :
 
-             alu_op == `ALU_OP_SET_LESS_THAN   ? $signed(A) < $signed(B)   :
+             alu_op == `ALU_OP_SET_LESS_THAN_U ? {1'b0,A < B,1'b0}                    :
 
-             alu_op == `ALU_OP_AND             ? A & B                     :
+             alu_op == `ALU_OP_SET_LESS_THAN   ? {1'b0,$signed(A) < $signed(B),1'b0} :
 
-             alu_op == `ALU_OP_OR              ? A | B                     :
+             alu_op == `ALU_OP_AND             ? {1'b0,A & B,1'b0}                     :
 
-             alu_op == `ALU_OP_XOR             ? A ^ B                  :
+             alu_op == `ALU_OP_OR              ? {1'b0,A | B,1'b0}                     :
 
-                               {DATA_WIDTH{1'b0}}; 
+             alu_op == `ALU_OP_XOR             ? {1'b0,A ^ B,1'b0}                  :
 
+                               {DATA_WIDTH+2{1'b0}}; 
+
+assign carry = alu_op == `ALU_OP_PLUS          ||
+               alu_op == `ALU_OP_SUB           ||
+               alu_op == `ALU_OP_SHIFT_LEFT     ? left :
+               alu_op == `ALU_OP_SHIFT_RIGHT   ||
+               alu_op == `ALU_OP_SHIFT_RIGHT_A  ? right :
+               1'b0;
 
 endmodule
